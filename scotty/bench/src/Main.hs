852ed064737f16d76191b72d3e6d2cf7d0fc1f7b
Andrew Rademacher <andrewrademacher@gmail.com>
2013-12-13 11:21:05 -0600

    Added plaintext benchmark to scotty.

--- scotty/bench/src/Main.hs
+++ scotty/bench/src/Main.hs
@@ -7,7 +7,11 @@ import           Data.Aeson ((.=), object)
 main :: IO ()
 main = scotty 3000 $ do
     get "/json" getJson
+    get "/plaintext" getPlainText
 
 getJson :: ActionM ()
 getJson = json $ object ["message" .= msg]
     where msg = "Hello, World!" :: Text
+
+getPlainText :: ActionM ()
+getPlainText = text "Hello, World!"
